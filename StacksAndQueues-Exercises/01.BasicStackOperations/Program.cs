dab32b48cb8d307bf4f1cd4bb82ace9079b3cc03
oksanakney <oksanakney@gmail.com>
2023-11-11 18:03:52 +0200

    Add exercises for Stacks And Queues
    9 exercises for Stacks And Queues

---
+++ StacksAndQueues-Exercises/01.BasicStackOperations/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Exercise_StacksAndQueues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tokens = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int elementsToPush = tokens[0];
+            int elementsToPop = tokens[1];
+            int number = tokens[2];
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < elementsToPush; i++) 
+            {
+                stack.Push(numbers[i]);
+            }
+
+            for (int i = elementsToPop; i > 0; i--)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Contains(number))
+            {
+                Console.WriteLine("true");
+            }
+            if (!stack.Contains(number) && stack.Any())
+            {
+                int min = stack.Min();
+                Console.WriteLine(min);
+            }
+            else if(!stack.Any())
+            {
+                Console.WriteLine("0");
+            }      
+        }
+    }
+}
---
+++ StacksAndQueues-Exercises/02.BasicQueueOperations/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _02.BasicQueueOperations
+{
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tokens = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int elementsToPush = tokens[0];
+            int elementsToPop = tokens[1];
+            int number = tokens[2];
+
+            Queue<int> queue = new Queue<int>();
+
+            for (int i = 0; i < elementsToPush; i++)
+            {
+                queue.Enqueue(numbers[i]);
+            }
+
+            for (int i = elementsToPop; i > 0; i--)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Contains(number))
+            {
+                Console.WriteLine("true");
+            }
+            if (!queue.Contains(number) && queue.Any())
+            {
+                int min = queue.Min();
+                Console.WriteLine(min);
+            }
+            else if (!queue.Any())
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
