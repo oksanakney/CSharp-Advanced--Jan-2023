d5f2b8ddf59e4ec143a478a05dd7f0d1b069cb49
oksanakney <oksanakney@gmail.com>
2023-12-05 16:24:58 +0200

    ExamPrep-03-Computer-Architecture
    03.Computer Architecture

---
+++ CSharp-Avd-ExamPrep-Classes03/Computer_Architecture/Computer.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ComputerArchitecture
+{
+    public class Computer
+    {
+        private List<CPU> multiprocessor;
+        private int capacity;
+        private string model;
+        public Computer(string model, int capacity)
+        {
+            Model = model;
+            Capacity = capacity;
+            this.Multiprocessor = new List<CPU>();
+        }
+        public List<CPU> Multiprocessor 
+        {
+            get 
+            {
+                return multiprocessor;
+            } 
+            set 
+            { 
+                multiprocessor = value;
+            } 
+        }
+        public int Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+            set
+            {
+                capacity = value;
+            }
+        }
+        public string Model
+        {
+            get
+            {
+                return model;
+            }
+            set
+            {
+                model = value;
+            }
+        }
+
+        
+
+        public int Count { get { return Multiprocessor.Count; } }
+
+        public void Add(CPU cpu)
+        {
+            if (Multiprocessor.Count < Capacity)
+            {
+                Multiprocessor.Add(cpu);
+            }
+        }
+
+        public bool Remove(string brand)
+        {
+            CPU cpuToRemove = Multiprocessor.FirstOrDefault(x => x.Brand == brand);
+            if (cpuToRemove != null) 
+            { 
+                return Multiprocessor.Remove(cpuToRemove);
+            }
+            return false;
+        }
+
+        //Method MostPowerful() - returns the most powerful CPU
+        //(the CPU with the highest frequency)
+
+        public CPU MostPowerful()
+        {
+            if (Multiprocessor.Count == 0)
+            {
+                return null;
+            }
+
+            CPU mostPowerful = Multiprocessor[0];            
+
+            foreach(var cpu in Multiprocessor)
+            {
+                if (cpu.Frequency > mostPowerful.Frequency)
+                {
+                    mostPowerful = cpu;
+                }
+            }
+            return mostPowerful;
+        }
+
+        //Method GetCPU(string brand) – returns the CPU with the given brand.
+        //If there is no CPU, meeting the requirements, return null
+
+        public CPU GetCPU(string brand)
+        {
+            return Multiprocessor.FirstOrDefault(c => c.Brand == brand);
+        }
+
+        public string Report()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"CPUs in the Computer {Model}:");
+            foreach(var cpu in Multiprocessor) 
+            { 
+                result.AppendLine(cpu.ToString());
+            }
+
+            return result.ToString().Trim();
+        }
+    }
+
+
+}
